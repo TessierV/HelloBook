9923892b535a42eab1c5aab1606f2e740658bcff
unknown <5628@holbertonstudents.com>
2023-09-20 18:34:39 +0200

    colors file

---
+++ colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+    mainBackground: '#F4F4F4',
+    lightBackground: '#E9EBF4',
+    darkBackground: '#29303E',
+
+    lightText: '#F5F5F1',
+    darkText: '#333639',
+
+    primaryColors: '#A0DC00',
+
+  };
