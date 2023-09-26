efbe40f3b4f7e0c3bfdcfc294aabf29bbdbf7952
unknown <5628@holbertonstudents.com>
2023-09-27 00:26:12 +0200

    fixing some issu

--- App.js
+++ App.js
@@ -1,70 +1,139 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView, TouchableOpacity, TextInput, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TouchableOpacity, Image } from 'react-native';
 import RecipeList from './component/RecipeList';
 import RecipeHeader from './component/RecipeHeader';
+import RecipeWorldHeader from './component/RecipeWorldHeader';
+import StreetFoodRecipeContainer from './component/StreetFoodRecipeContainer';
 import { colors } from './colors';
-import { recipes, categories } from './constant';
+import { recipes, categories, categories_World, categoryImages, selectedCategoryImages } from './constant';
+import SearchBar from './component/RecipeSearchBar';
+import CategoryList from './component/RecipeFilter';
+import DessertRecipeContainer from './component/DessertRecipeContainer';
+import AnnouncementButton from './component/AnnouncementButton';
+import AnnouncementBuilding from './component/AnnouncementBuilding';
+import CategoryList_World from './component/RecipeFilter_World';
+import BrunchRecipeContainer from './component/BrunchRecipeContainer';
+import NewRecipeContainer from './component/NewRecipeContainer';
+import WorldRecipeHeader from './component/WorldRecipeHeader';
 
 export default function App() {
   const [searchTerm, setSearchTerm] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('All');
   const [likedRecipes, setLikedRecipes] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [selectedCategoryWorld, setSelectedCategoryWorld] = useState('Asie'); // Catégorie pour RecipeListWorld
 
   const filteredRecipes = recipes.filter((recipe) => {
-    if (selectedCategory !== 'All' && recipe.course !== selectedCategory) {
+    if (selectedCategory !== 'All' && !recipe.course.includes(selectedCategory)) {
       return false;
     }
     return recipe.title.toLowerCase().includes(searchTerm.toLowerCase());
   });
 
-  const toggleLikeRecipe = (recipe) => {
-    if (likedRecipes.includes(recipe)) {
-      setLikedRecipes(likedRecipes.filter((item) => item !== recipe));
+  const filteredRecipesWorld = recipes.filter((recipe) => {
+    if (selectedCategoryWorld !== 'All' && !recipe.course.includes(selectedCategoryWorld)) {
+      return false;
+    }
+    return recipe.title.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
+  const toggleLikeRecipe = (recipeIndex) => {
+    if (likedRecipes.includes(recipeIndex)) {
+      setLikedRecipes(likedRecipes.filter((likedRecipe) => likedRecipe !== recipeIndex));
     } else {
-      setLikedRecipes([...likedRecipes, recipe]);
+      setLikedRecipes([...likedRecipes, recipeIndex]);
     }
   };
 
-  const renderCategoryButton = (category) => (
-    <TouchableOpacity
-      key={category}
-      style={[
-        styles.categoryButton,
-        selectedCategory === category && styles.selectedCategory,
-      ]}
-      onPress={() => setSelectedCategory(category)}
-    >
-      <Text>{category}</Text>
-    </TouchableOpacity>
-  );
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category); // Mettre à jour la catégorie globale
+  };
+
+  const handleCategoryChangeWorld = (category) => {
+    setSelectedCategoryWorld(category); // Mettre à jour la catégorie pour RecipeListWorld
+  };
+
+  const renderCategoryButton = (category, selectedCategory, onPress) => {
+    const categoryImage =
+      selectedCategory === category
+        ? selectedCategoryImages[category]
+        : categoryImages[category];
+
+    return (
+      <TouchableOpacity
+        key={category}
+        style={[
+          styles.categoryButton,
+          selectedCategory === category && styles.selectedCategory,
+        ]}
+        onPress={onPress}
+      >
+        <Image source={categoryImage} style={styles.categoryIcon} />
+        <Text style={{ fontSize: 11, textAlign: 'center', color: colors.darkBackground }}>{category}</Text>
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View style={styles.maincontainer}>
-      <View style={styles.container}>
-        <RecipeHeader />
-        <View style={styles.searchBar}>
-          <Image source={require('./img/search.png')} style={styles.searchIcon} />
-          <TextInput
-            style={styles.searchInput}
-            placeholderTextColor="#F5F5F1"
-            placeholder="Search by Title"
-            value={searchTerm}
-            onChangeText={(text) => setSearchTerm(text)}
+      <ScrollView>
+        <View style={styles.container}>
+          <RecipeHeader />
+          <SearchBar value={searchTerm} onChangeText={(text) => setSearchTerm(text)} />
+
+
+
+          <CategoryList
+            categories={categories}
+            selectedCategory={selectedCategory}
+            renderCategoryButton={(category) => renderCategoryButton(category, selectedCategory, () => handleCategoryChange(category))}
           />
+          <RecipeList
+            recipes={filteredRecipes}
+            likedRecipes={likedRecipes}
+            onToggleLike={toggleLikeRecipe}
+            isLast={true}
+          />
+          <RecipeWorldHeader />
+          <NewRecipeContainer recipes={recipes}/>
+
+          <BrunchRecipeContainer
+            recipes={recipes}
+            likedRecipes={likedRecipes}
+            onToggleLike={toggleLikeRecipe}
+          />
+          <StreetFoodRecipeContainer
+            recipes={recipes}
+            likedRecipes={likedRecipes}
+            onToggleLike={toggleLikeRecipe}
+          />
+          <DessertRecipeContainer
+            recipes={recipes}
+            likedRecipes={likedRecipes}
+            onToggleLike={toggleLikeRecipe}
+          />
+
+          <View>
+          <WorldRecipeHeader />
+
+            <CategoryList_World
+              categories={categories_World}
+              selectedCategory={selectedCategoryWorld}
+              renderCategoryButton={(category) => renderCategoryButton(category, selectedCategoryWorld, () => handleCategoryChangeWorld(category))}
+            />
+
+            <RecipeList
+              recipes={filteredRecipesWorld}
+              likedRecipes={likedRecipes}
+              onToggleLike={toggleLikeRecipe}
+              isLast={true}
+            />
+          </View>
         </View>
-        <ScrollView
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={styles.categoryBar}
-        >
-          {categories.map((category) => renderCategoryButton(category))}
-        </ScrollView>
-        <RecipeList
-          recipes={filteredRecipes}
-          likedRecipes={likedRecipes}
-          onToggleLike={toggleLikeRecipe}
-          isLast={true}
-        />
+      </ScrollView>
+      <View style={styles.categoryFooter}>
+        <AnnouncementButton />
+        <AnnouncementBuilding test="Favoris" />
+        <AnnouncementBuilding test="Reglage" />
       </View>
     </View>
   );
@@ -78,41 +147,43 @@ const styles = StyleSheet.create({
   },
   container: {
     justifyContent: 'center',
-    width: "90%",
+    width: '90%',
     alignSelf: 'center',
-    marginBottom: "10%",
-  },
-  searchBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 20,
-    borderRadius: 10,
-    backgroundColor: colors.darkBackground,
-  },
-  searchIcon: {
-    width: 24,
-    height: 24,
-    marginHorizontal: 10,
-  },
-  searchInput: {
-    flex: 1,
-    padding: 10,
+    marginBottom: '10%',
   },
   categoryBar: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: 45,
     marginBottom: '4%',
+    justifyContent: 'center',
+    alignSelf: 'center',
+  },
+  categoryFooter: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 0,
+    height: 60,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: colors.mainBackground,
+    width: '100%',
+    borderTopEndRadius: 15,
+    borderTopLeftRadius: 15,
+    elevation: 6,
+  },
+  categoryIcon: {
+    width: 30,
+    height: 30,
+    marginBottom: 5,
   },
   categoryButton: {
-    borderRadius: 5,
-    marginRight: 10,
-    marginBottom: 10,
-    paddingHorizontal: 10,
-    justifyContent: 'center',
+    marginBottom: 5,
+    alignItems: 'center',
   },
-  selectedCategory: {
-    borderBottomWidth: 1,
-    borderBottomColor: colors.darkBackground,
+  categoryHeaderText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 10,
   },
 });
