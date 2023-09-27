7490f992999743d939ac458e4359eef0104b068e
unknown <5628@holbertonstudents.com>
2023-09-27 17:43:14 +0200

    add the second color

--- colors.js
+++ colors.js
@@ -2,11 +2,13 @@ export const colors = {
     mainBackground: '#F4F4F4',
     lightBackground: '#E9EBF4',
     darkBackground: '#29303E',
+    colorBackground: "#D86F52",
 
     lightText: '#E9EBF4',
     darkText: '#333639',
     greyText: '#7D8291',
 
     primaryColors: '#A0DC00',
+    secondColors: "#D86F52",
 
   };
